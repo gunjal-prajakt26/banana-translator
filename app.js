f39448fa26d85a05727c80b975b22304de7a346b
gunjal-prajakt26 <gunjal.prajakt951@gmail.com>
2022-01-09 14:22:08 +0530

    updated working is done

---
+++ app.js
@@ -0,0 +1,26 @@
+var inputText = document.querySelector("textarea");
+var btnTranslator = document.querySelector("#translate-btn");
+var outputText = document.querySelector(".output-area");
+
+serverUrl = "https://api.funtranslations.com/translate/minion.json";
+
+function readText(text) {
+    return serverUrl + "?text=" + text;
+}
+
+function errorHandler(error){
+    console.log("Error is occured type error :"+ error);
+}
+
+btnTranslator.addEventListener("click", function clickEventHandler() {
+
+    var textInput = inputText.value;
+
+    fetch(readText(textInput))
+        .then(Response => Response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputText.innerText= translatedText;
+            }) 
+        .catch(errorHandler)
+})
